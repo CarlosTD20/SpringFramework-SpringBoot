7bb72950332b57a0d1638e0753fd7937e19bf865
CarlosTD20 <carlostornero204@gmail.com>
2025-06-11 12:34:48 +0200

    Sección 16: Deply en Apache Tomcat 10 Externo

--- springboot-crud/src/main/java/com/carlos/curso/springboot/app/springboot_crud/SpringbootCrudApplication.java
+++ springboot-crud/src/main/java/com/carlos/curso/springboot/app/springboot_crud/SpringbootCrudApplication.java
@@ -2,10 +2,11 @@ package com.carlos.curso.springboot.app.springboot_crud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-public class SpringbootCrudApplication {
+public class SpringbootCrudApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootCrudApplication.class, args);
